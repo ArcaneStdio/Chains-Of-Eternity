7281614b6c1fc6bcd26b66eea87f619abd934dc7
Vinayak-RZ <vinayakraizada@gmail.com>
2025-10-05 23:24:29 +0530

    feat(muliplayer): adds player color handler for multiplayer

--- unity/Assets/PurrNet/Runtime/Transports/PurrTransportUtils.cs
+++ unity/Assets/PurrNet/Runtime/Transports/PurrTransportUtils.cs
@@ -136,7 +136,7 @@ namespace PurrNet.Transports
             var request = UnityWebRequest.Get(url);
             request.useHttpContinue = false;
             request.SetRequestHeader("Cache-Control", "no-cache");
-            request.SetRequestHeader("region", "france");
+            request.SetRequestHeader("region", "china");
             request.SetRequestHeader("name", roomName);
             var response = await request.SendWebRequest();
 
